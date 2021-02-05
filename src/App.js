84ac3c864fd5663e3aa726d5a48a82e3be01f36a
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-02-05 14:27:24 -0300

    sexto deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 
+import Logo from './components/logo-tr.png'
+
 import './App.css';
 
 export default () => {
     return(
-		<div className="a" >Mano, deu certo pra calho vc nao faz ideiaaaaaaaa</div>
+		<div className="app">
+			<header>
+				<img className="logo-design" src={Logo} />
+			</header>
+
+			<div className="titulo-principal-1">
+				<h2>Aprenda <small className="chamar-atencao">de forma rápida</small> a criar seus designs<small className="pontos">.</small></h2>
+			</div>
+
+
+
+    	</div>
     )
 }
