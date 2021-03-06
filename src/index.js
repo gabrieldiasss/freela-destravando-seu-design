b2d431e541c2c98efce93414fcd7c89363540401
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-03-06 14:39:32 -0300

    115 deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { hydrate, render } from "react-dom";
+
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 ReactDOM.render(
   <React.StrictMode>
